2ff5dfc5dc832aa048b99ce96e5946d9976fe927
Shiva Satapathy <shivasatapathy99@gmail.com>
2020-11-30 17:38:55 +0530

    url added

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,7 @@
 var UNIVERSAL = {
     // BASEURL: "http://localhost:8008/"
-    BASEURL: "https://skyyrider.herokuapp.com/"
+    BASEURL: "https://taxopliance.herokuapp.com/"
         // BASEURL: "http://192.168.1.2:8008/"
         // BASEURL: "http://192.168.0.165:8008/"
-        //BASEURL: "http://192.168.0.136:8008/"
-        // BASEURL: "http://192.168.0.191:8008/"
 };
 export default UNIVERSAL;

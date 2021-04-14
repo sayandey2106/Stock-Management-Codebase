47d8351e69ce4d7f487288fe8d8ff99af5d01392
Abhishek Mishra <abhishek.pikun@gmail.com>
2021-04-14 22:45:15 +0530

    dashboard ui pushed

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const UNIVERSAL = {
     // BASEURL: "http://localhost:8000/"
-    // BASEURL: "https://qubi7.herokuapp.com/"
-        BASEURL: "http://192.168.29.199:8000/"
+    BASEURL: "https://qubi7.herokuapp.com/"
+        // BASEURL: "http://192.168.29.199:8000/"
         // BASEURL: "http://192.168.0.165:8008/"
 };
 export default UNIVERSAL;

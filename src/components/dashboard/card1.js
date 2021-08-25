68b44d785a715527624dd154aa5adf9627e708b3
Sabyasachi Rout <routsabyasachi1@gmail.com>
2021-08-25 09:43:48 +0530

    ..

--- src/components/dashboard/card1.js
+++ src/components/dashboard/card1.js
@@ -7,11 +7,14 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogActions from "@material-ui/core/DialogActions";
 import TextField from "@material-ui/core/TextField";
+import pay from "./razorpay";
 
 function Card1(props) {
   const [open, setOpen] = useState(false);
-  var icon;
-  if (props.Button === true) {
+  const [price, setPrice] = useState("");
+  const [helper, setHelper] = useState("Atleast ₹2,500/- to be paid");
+    let icon;
+    if (props.Button === true) {
     
     icon = <button onClick={() => setOpen(true)}>PAY</button>
   }
@@ -24,27 +27,45 @@ function Card1(props) {
       <Dialog open={open}>
         <DialogTitle> payment</DialogTitle>
         <DialogContent>
-          <TextField
+         {/* <TextField
             margin="dense"
             label="Name"
             type="text"
             fullWidth
             // onChange={}
             // value={}
-          />
+          />*/}
 
           <TextField
-            margin="dense"
+            /*margin="dense"
             label="Amount"
             type="text"
-            fullWidth
+            fullWidth*/
             // onChange={}
             // value={}
+              error
+              id="filled-error-helper-text"
+              label="Amount"
+              // defaultValue="Hello World"
+              helperText={helper}
+              variant="filled"
+              onchange={(event) => {
+                  if(event.target.value >= 2500){
+                      setHelper("");
+                      setPrice(event.target.value);
+                  }
+              }}
           />
         </DialogContent>
         <DialogActions>
           <Button variant="contained" color="secondary" onClick={() => setOpen(false)}>close</Button>
-          <Button variant="contained" color="primary" onClick={() => setOpen(false)} >proceed</Button>
+          <Button variant="contained" color="primary" onClick={() => {
+              setOpen(false);
+              if(price >= 2500) {
+                  pay((price * 105) / 100);
+              }
+          }}
+          >proceed</Button>
         </DialogActions>
       </Dialog>
     </div>
---
+++ src/components/dashboard/razorpay.js
@@ -0,0 +1,79 @@
+// let _DEV_ = document.domain === "localhost";
+const key = "rzp_live_lMlEdUdYhkoR8x";
+
+const loadScript = (src) => {
+    return new Promise((resolve => {
+        const script = document.createElement('script');
+        script.src = src
+        document.body.appendChild(script);
+        script.onload = () => {
+            resolve(true)
+        }
+        script.onerror = () => {
+            resolve(false)
+        }
+    }))
+}
+
+
+async function pay(description, product_id, machineName) {
+    console.log("Payed successfully" + description)
+    const res = await loadScript("https://checkout.razorpay.com/v1/checkout.js")
+    if (!res) {
+        alert("RazorPay SDK failed to load. Check Your Internet Connection")
+        return
+    }
+
+    let data = await fetch("https://server-bumqe7vyuq-el.a.run.app/create_order", {
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    }).then(resp => resp.json())
+        .catch(console.log)
+    console.log(data)
+
+    const options = {
+        "key": key,
+        "amount": 11*100, // 100p = 1rs / 100cents = 1$
+        "currency": data.currency,
+        "name": "QUBI 7",
+        "description": "SCAN QR " + description,
+        // "image": details.image,
+        // "image": "https://firebasestorage.googleapis.com/v0/b/kiosk-72887.appspot.com/o/output-onlinepngtools.png?alt=media&token=f1002906-cc77-472c-b819-5d33b24fa973",
+        "order_id": data.id,
+
+        // "handler": function (response) {
+        //     alert("payment_id: " + response.razorpay_payment_id);
+        //     alert("order_id: " + response.razorpay_order_id);
+        //     alert("signature: " + response.razorpay_signature);
+        // },
+
+        "prefill": {
+            "name": "Nihar Panda",
+            "email": "no-reply@qubi7.com",
+            "contact": 911000000000,
+            "method": "upi",
+            "upi":
+            {
+                "flow": 'intent'
+            }
+        },
+        "theme": {
+            // color: "green"
+        },
+        "modal": {
+            escape: true,
+        },
+        "notes": {
+            product_id: product_id,
+            machineName: machineName,
+            // type:'D'
+        }
+
+    };
+    const paymentObject = new window.Razorpay(options)
+    paymentObject.open();
+}
+
+export default pay;

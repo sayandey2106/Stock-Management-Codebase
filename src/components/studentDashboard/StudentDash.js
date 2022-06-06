af23468431581e3ab0e6d2b9e98aa3cbf631886c
Your Name <vishaldurge567@gmail.com>
2022-06-06 20:16:34 +0530

    Dashboard Update with The header bar

--- src/components/studentDashboard/StudentDash.js
+++ src/components/studentDashboard/StudentDash.js
@@ -1,56 +1,57 @@
 import React from "react";
 import "./style.css";
-
 // import { Link } from 'react-router-dom'
 
 function StudentDash() {
   return (
+    
     <div className="container">
-
       {/* Button LOGOUT */}
-      <div className="log-out">
+      <div className="btn-wraper">
+        <div className="log-out">
           <div className="dropdown">
-            <button className="dropbtn">User Name</button>
+            <button className="dropbtn">HI, User Name</button>
             <div className="dropdown-content">
-            <a href="/" >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-user align-middle me-1"
-              >
-                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>{" "}
-              Profile
-            </a>
-            <a href="/" >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-power align-middle me-1"
-              >
-                <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
-                <line x1="12" y1="2" x2="12" y2="12"></line>
-              </svg>
-              Log out
-            </a>
-              <a href="/">Link 3</a>
+              <a href="/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-user align-middle me-1"
+                >
+                  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </svg>{" "}
+                Profile
+              </a>
+              <a href="/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-power align-middle me-1"
+                >
+                  <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
+                  <line x1="12" y1="2" x2="12" y2="12"></line>
+                </svg>
+                Log out
+              </a>
+              <a href="/">Change Password</a>
             </div>
           </div>
+        </div>
       </div>
 
       {/* DETAILED CARDS */}
@@ -66,10 +67,18 @@ function StudentDash() {
             </div>
           </div>
           <div className="details">
-            <h3>Credits <span>:</span></h3>
-            <h3>Percentage <span>:</span></h3>
-            <h3>CGPA <span>:</span></h3>
-            <h3>SGPA <span>:</span></h3>
+            <h3>
+              Credits <span>:</span>
+            </h3>
+            <h3>
+              Percentage <span>:</span>
+            </h3>
+            <h3>
+              Rank <span>:</span>
+            </h3>
+            <h3>
+              Grade <span>:</span>
+            </h3>
           </div>
         </section>
 
